1b3c2bb9693a9378b110e523867b1ec0fc749f00
Dharti savaliya <rutvikasavliya041@gmail.com>
2025-03-13 15:54:18 +0530

    Add menu in Navbar.jsx file

--- delivery/src/components/Navbar/Navbar.jsx
+++ delivery/src/components/Navbar/Navbar.jsx
@@ -4,21 +4,27 @@ import { assets } from '../../assets/assets'
 
 const Navbar = () => {
     return (
+        // header
         <div className='navbar'>
 
             <img src={assets.logo} alt="" className='logo' />
             <ul className="navbar_menu">
                 <li>home</li>
                 <li>menu</li>
+                <li>mobile-app</li>
                 <li>Contact us</li>
             </ul>
 
+            {/*  */}
             <div className='navbar_right'>
-
                 <img src={assets.search_icon} alt="" />
+                <div className='navbar-search-icon'>
+                    <img src={assets.basket_icon} alt="" />
+                    <div className='dot'></div>
+                </div>
 
                 <div>
-
+                    <button>sing in</button>
                 </div>
 
             </div>
