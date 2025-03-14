829ef19796fd4d11e275b15b2d340a238db2a654
Dharti savaliya <rutvikasavliya041@gmail.com>
2025-03-14 17:00:44 +0530

    Add router is App.jsx file

--- delivery/src/App.jsx
+++ delivery/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Router } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
+
+      <Router>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<PlaceOrder />} />
+      </Router>
 
     </div>
   )
