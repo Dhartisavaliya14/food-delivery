dab84fa46a377e5be1e589a5fbe80cc7a4cfd768
Dharti savaliya <rutvikasavliya041@gmail.com>
2025-03-15 22:00:02 +0530

    Add logo

--- delivery/src/context/StoreContext.jsx
+++ delivery/src/context/StoreContext.jsx
@@ -1,12 +1,39 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
 
+    // Add to cart section
+
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = (itemId) => {
+
+        if (!cartItems[itemId]) {
+            setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
+        }
+        else {
+            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
+        }
+
+    }
+
+    // Remove to Cart item
+
+    const removeFromCart = (itemId) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+    }
+
+    // passing food list
+
     const contextValue = {
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart,
     }
 
     return (
