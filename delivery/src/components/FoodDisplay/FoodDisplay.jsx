d0d32b4cab00191131e45c6c24e97c9318dcd2c4
Dharti savaliya <rutvikasavliya041@gmail.com>
2025-03-15 14:34:58 +0530

    Create API in StoreContext.jsp file

---
+++ delivery/src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './FoodDisplay.css'
+
+const FoodDisplay = () => {
+
+    const { }
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default FoodDisplay
