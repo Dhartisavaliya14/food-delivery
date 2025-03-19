bbabd6bb8f79aec4bc9e3913c99a0ded3c955685
Dharti savaliya <rutvikasavliya041@gmail.com>
2025-03-19 18:51:36 +0530

    Add to cart functionality

--- delivery/src/components/Navbar/Navbar.jsx
+++ delivery/src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = ({ setShowLogin }) => {
         // left side navbar menu
         <div className='navbar'>
 
-            <img src={assets.logo} alt="" className='logo' />
+            <Link to='/'><img src={assets.logo} alt="" className='logo' /></Link>
             <ul className="navbar_menu">
                 <Link to='/' onClick={() => setMenu("Home")} className={menu === "Home" ? "active" : ""}>Home</Link>
                 <a href='#explore-menu' onClick={() => setMenu("Menu")} className={menu === "Menu" ? "active" : ""}>Menu</a>
@@ -22,7 +22,7 @@ const Navbar = ({ setShowLogin }) => {
             <div className='navbar_right'>
                 <img src={assets.search_icon} alt="" />
                 <div className='navbar-search-icon'>
-                    <img src={assets.basket_icon} alt="" />
+                    <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
                     <div className='dot'></div>
                 </div>
 
