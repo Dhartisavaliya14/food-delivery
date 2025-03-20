dacbc77a0e79457351480478e899c0efbf1208ea
Dharti savaliya <rutvikasavliya041@gmail.com>
2025-03-20 17:52:32 +0530

    Add to cart

--- delivery/src/pages/Cart/Cart.jsx
+++ delivery/src/pages/Cart/Cart.jsx
@@ -9,6 +9,8 @@ const Cart = () => {
 
     return (
         <div className='cart'>
+
+            {/* cart item title */}
             <div className="cart-items">
                 <div className='cart-items-title'>
                     <p>Items</p>
@@ -20,6 +22,8 @@ const Cart = () => {
                 </div>
                 <br />
                 <hr />
+
+                {/* show food items  */}
                 {food_list.map((item, index) => {
                     if (cartItems[item._id] > 0) {
                         return (
@@ -42,6 +46,42 @@ const Cart = () => {
                 })}
             </div>
 
+            {/* cart total section */}
+            <div className="cart-bottom">
+                <div className='cart-total'>
+                    <h2>Cart Total</h2>
+                    <div>
+                        <div className="cart-total-details">
+                            <p>Subtotal</p>
+                            <p>{0}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <p>Delivery Fee</p>
+                            <p>{2}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <b>Total</b>
+                            <b>{0}</b>
+                        </div>
+
+                    </div>
+                    <button>PROCEED TO CHECKOUT</button>
+                </div>
+
+
+                {/* cart promocode */}
+                <div className="cart-promocode">
+                    <div>
+                        <p>If you have promo code, Enter it here</p>
+                        <div className="cart-promocode-input">
+                            <input type="text" placeholder='Promocode' />
+                            <button>Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
