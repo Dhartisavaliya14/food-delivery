b006313b5927b835c422e90839eef0b076c5b71c
Dharti savaliya <rutvikasavliya041@gmail.com>
2025-03-30 09:50:41 +0530

    create navbar for admin

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './Sidebar.css'
+import { assets } from '../../assets/assets'
 
 const Sidebar = () => {
     return (
-        <div>
+        <div className='sidbar'>
+
+            <div className="sidbar-options">
+                <div className="sidbar-option">
+                    <img src={assets.add_icon} alt="" />
+                    <p>Add Items</p>
+                </div>
+
+                <div className="sidbar-option">
+                    <img src={assets.order_icon} alt="" />
+                    <p>List Items</p>
+                </div>
+
+                <div className="sidbar-option">
+                    <img src={assets.order_icon} alt="" />
+                    <p>Orders</p>
+                </div>
+            </div>
 
         </div>
     )
